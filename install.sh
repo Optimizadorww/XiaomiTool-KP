b221505232d68fbf1c2dbb0e392a1582c0e8540b
KP <lozadaerwin242@gmail.com>
2026-01-08 00:13:28 -0400

    Update installation script for XiaomiTool-KP

--- install.sh
+++ install.sh
@@ -1,27 +1,19 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
-echo -e "\033[0;32mInstalando XiaomiTool KP de @AntiKripis...\033[0m"
-
-# Dar permisos de almacenamiento a Termux
-termux-setup-storage
+echo -e "\e[32mInstalando XiaomiTool-KP de @AntiKripis...\e[0m"
 
 # Instalar dependencias necesarias
 pkg update && pkg upgrade -y
-pkg install android-tools git python wget -y
+pkg install android-tools curl python -y
 
-# Descargar el script principal desde tu repo
-# REEMPLAZA TU_USER Y TU_REPO con tus datos reales de GitHub
-curl -sL https://raw.githubusercontent.com/TU_USER/TU_REPO/main/XiaomiTool.sh -o XiaomiTool.sh
+# Descargar el script principal y meterlo en el sistema
+# NOTA: Cambia 'TU_USER' por tu nombre real de GitHub abajo
+curl -sL https://raw.githubusercontent.com/TU_USER/XiaomiTool-KP/main/XiaomiTool.sh -o $PREFIX/bin/XiaomiTool-KP
 
-# DAR TODOS LOS PERMISOS
-chmod 777 XiaomiTool.sh
+# DAR TODOS LOS PERMISOS (Lectura, Escritura, Ejecución)
+chmod 777 $PREFIX/bin/XiaomiTool-KP
 
-# Mover a la carpeta de ejecución global
-mv XiaomiTool.sh $PREFIX/bin/XiaomiTool
-chmod +x $PREFIX/bin/XiaomiTool
-
-echo -e "\033[0;36m--------------------------------------------------\033[0m"
-echo -e "\033[0;32m¡TODO LISTO Y CON PERMISOS!\033[0m"
-echo -e "Ahora solo escribe: \033[1;33mXiaomiTool\033[0m"
-echo -e "\033[0;36m--------------------------------------------------\033[0m"
-
+echo -e "\e[36m--------------------------------------------------\e[0m"
+echo -e "\e[32m¡INSTALACIÓN COMPLETADA CON ÉXITO!\e[0m"
+echo -e "Ya puedes usar la herramienta escribiendo: \e[33mXiaomiTool-KP\e[0m"
+echo -e "\e[36m--------------------------------------------------\e[0m"
